f6c9eca041521fbfecf91c41b25da3d5d52e78cc
Jason Chang <kenzz0979288190@gmail.com>
2020-01-08 17:52:13 +0800

    update

---
+++ IndoorNavigation/IndoorNavigation/IWifiConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndoorNavigation
+{
+    public interface IWifiConnect
+    {
+        void ConnectToWifi(string ssid, string password);
+        void DisconnectToWifi();
+
+    }
+}
