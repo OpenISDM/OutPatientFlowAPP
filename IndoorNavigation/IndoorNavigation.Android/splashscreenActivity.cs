8964667bae9f44f890567e45b2c536e33ecebea3
Jason Chang <kenzz0979288190@gmail.com>
2020-08-16 17:55:14 +0800

    to put static splash screen in android project

--- IndoorNavigation/IndoorNavigation.Android/splashscreenActivity.cs
+++ IndoorNavigation/IndoorNavigation.Android/splashscreenActivity.cs
@@ -2,16 +2,18 @@
 using Android.Content;
 using Android.Content.PM;
 using Android.OS;
+using Android.Support.V7.App;
 using Android.Widget;
 using System;
+using System.Security.Permissions;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace IndoorNavigation.Droid
 {
-    [Activity(Theme = "@android:style/Theme.NoTitleBar", MainLauncher = true, 
-        NoHistory =true, ScreenOrientation = ScreenOrientation.Portrait)]
-    public class splashscreenActivity : Activity
+    [Activity(Theme = "@style/splashscreen", MainLauncher = true, 
+     ScreenOrientation = ScreenOrientation.Portrait)]
+    public class splashscreenActivity : AppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -19,25 +21,28 @@ namespace IndoorNavigation.Droid
 
             SetContentView(Resource.Layout.splashscreen);
 
+            Intent intent = new Intent(this, typeof(MainActivity));
+            StartActivity(intent);
+            Finish();
         }
 
 
-        protected override void OnResume()
-        {
-            base.OnResume();
-            Task startupWork = new Task(() => { SimulateStartUp(); });
-            startupWork.Start();
-        }
+        //protected override void OnResume()
+        //{
+        //    base.OnResume();
+        //    Task startupWork = new Task(() => { SimulateStartUp(); });
+        //    startupWork.Start();
+        //}
 
-        public override void OnBackPressed()
-        {            
-        }
+        //public override void OnBackPressed()
+        //{            
+        //}
 
-        async void SimulateStartUp()
-        {
-            await Task.Delay(3000);
-            StartActivity(new Intent(Application.Context, 
-                typeof(MainActivity)));
-        }
+        //async void SimulateStartUp()
+        //{
+        //    await Task.Delay(3000);
+        //    StartActivity(new Intent(Application.Context, 
+        //        typeof(MainActivity)));
+        //}
     }
 }
