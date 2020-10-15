8c5ccd55a3d8674d7efcd853757d484496e93d6c
Jason Chang <kenzz0979288190@gmail.com>
2020-10-15 13:29:56 +0800

    to hide TestPage Button in root page.

--- IndoorNavigation/IndoorNavigation/MainPage.xaml.cs
+++ IndoorNavigation/IndoorNavigation/MainPage.xaml.cs
@@ -448,7 +448,7 @@ namespace IndoorNavigation
 
             ToolbarItems.Add(SettingItem);
             ToolbarItems.Add(NewSiteToolbarItem);
-            ToolbarItems.Add(TestToolbarItem);
+            //ToolbarItems.Add(TestToolbarItem);
             OnToolbarItemAdded();
         }
         async private Task SettingItemMethod()
