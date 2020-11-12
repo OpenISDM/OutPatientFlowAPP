2a43408ed07bffa3cd14bc536de173ae4e0993fa
Jason Chang <kenzz0979288190@gmail.com>
2020-11-12 11:42:41 +0800

    start to refactor ips module

---
+++ IndoorNavigation/IndoorNavigation/Modules/IPSModule_.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using IndoorNavigation.Models;
+using IndoorNavigation.Models.NavigaionLayer;
+using IndoorNavigation.Modules.IPSClients;
+
+namespace IndoorNavigation.Modules
+{
+    public class IPSModule_ : IDisposable
+    {
+        private NavigationGraph _naviGraph;
+        public NavigationEvent _event { get; private set; }
+        private Dictionary<IPSType, IPSClient> _multiClients;
+        private HashSet<IPSType> OpenedIPSType;
+        public IPSModule_(NavigationGraph naviGraph)
+        {
+            Console.WriteLine(">>IPSmodule : constructor");
+
+            _naviGraph = naviGraph;
+            _event = new NavigationEvent();
+            OpenedIPSType = new HashSet<IPSType>();            
+
+            #region PS type initial
+            _multiClients.Add(IPSType.LBeacon, new IPSClient
+            {
+               type = IPSType.LBeacon,
+               client = new WaypointClient(),
+               _monitorBeaconMapping = new List<WaypointBeaconsMapping>()
+            });
+
+            _multiClients.Add(IPSType.iBeacon, new IPSClient
+            {
+                type = IPSType.iBeacon,
+                client = new IBeaconClient(),
+                _monitorBeaconMapping = new List<WaypointBeaconsMapping>()
+            });
+
+            //this is not supported now.
+            //_multiClient.Add(IPSType.GPS, new IPSClient
+            //{
+
+            //});
+            #endregion
+
+            Console.WriteLine("<<IPSmodule : constructor");
+        }
+        #region for detect waypoint
+        public void InitialStep_DetectAllBeacon(List<Guid> regionIDs)
+        {
+
+        }
+        #endregion
+
+        #region for detect rssi
+
+        private WaypointBeaconsMapping _rssiMapping;
+        private IPSType _rssiIPStype;
+
+        public void SetRssiMonitor(Guid regionID, Guid waypointID) 
+        {
+            _rssiMapping = GetSingleBeaconMapping(regionID, waypointID);
+            _rssiIPStype = _naviGraph.GetRegionIPSType(regionID);
+            _multiClients[_rssiIPStype].client._event._eventHandler +=
+                PassScanRssiValue;
+            _multiClients[_rssiIPStype].ContainType = true;
+        }
+        public void OpenRssiScanning() 
+        {
+            _multiClients[_rssiIPStype].client.OnRestart();
+            _multiClients[_rssiIPStype].client
+                .DetectWaypointRssi(_rssiMapping);
+        }
+        public void PassScanRssiValue(object sender, EventArgs args)
+        {
+            Console.WriteLine(">>IPSmodule : PassScanRssiValue");
+            _event.OnEventCall(args as WaypointRssiEventArgs);
+        }
+        #endregion
+
+        #region for get/parse data
+        private WaypointBeaconsMapping GetSingleBeaconMapping(Guid regionID,
+            Guid waypointID)
+        {
+            Console.WriteLine(">>IPSmodule : GetSingleBeaconMapping");
+
+            List<Guid> beaconIDs =
+                _naviGraph.GetAllBeaconIDInOneWaypointOfRegion(
+                    regionID, waypointID);
+
+            Dictionary<Guid, int> beaconThresholdMapping =
+                new Dictionary<Guid, int>();
+
+            foreach (Guid beaconID in beaconIDs)
+            {
+                beaconThresholdMapping.Add(beaconID,
+                    _naviGraph.GetBeaconRSSIThreshold(regionID, beaconID)
+                    );
+            }
+
+            return new WaypointBeaconsMapping
+            {
+                _Beacons = beaconIDs,
+                _BeaconThreshold = beaconThresholdMapping,
+                _WaypointIDAndRegionID = new RegionWaypointPoint
+                {
+                    _regionID = regionID,
+                    _waypointID = waypointID
+                }
+            };
+        }
+        private List<WaypointBeaconsMapping> GetBeaconMappingList(
+            Guid regionID, List<Guid> waypointIDs)
+        {
+            Console.WriteLine(">>IPSmodule : GetBeaconMappingList");
+            List<WaypointBeaconsMapping> BeaconWaypointMapping =
+                new List<WaypointBeaconsMapping>();
+
+            foreach (Guid waypointID in waypointIDs)
+            {
+                BeaconWaypointMapping.Add
+                    (GetSingleBeaconMapping(regionID, waypointID));
+            }
+
+            Console.WriteLine("<<IPSmodule : GetBeaconMappingList");
+            return BeaconWaypointMapping;
+        }
+        #endregion
+
+        #region for disposed function
+        private bool _disposedValue = false;
+        protected virtual void Dispose(bool disposing)
+        {
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        #endregion
+
+        #region Data Structure
+        public class IPSClient
+        {
+            public IPSType type { get; set; }
+            public bool ContainType { get; set; } = false;
+            public IIPSClient client { get; set; }
+            public List<WaypointBeaconsMapping> _monitorBeaconMapping { get; set; }
+        }
+        #endregion
+    }
+}
