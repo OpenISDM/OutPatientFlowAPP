70b0e99af64dbeaf61b3d6aed3cefd50b78059ff
Jason Chang <kenzz0979288190@gmail.com>
2020-12-30 10:49:51 +0800

    to add Deparment select page.

--- IndoorNavigation/IndoorNavigation/Views/PopUpPage/DepartmentPopupPage.xaml.cs
+++ IndoorNavigation/IndoorNavigation/Views/PopUpPage/DepartmentPopupPage.xaml.cs
@@ -8,14 +8,33 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 using Rg.Plugins.Popup.Pages;
+using CheckBox = Plugin.InputKit.Shared.Controls.CheckBox;
 namespace IndoorNavigation.Views.PopUpPage
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class DepartmentPopupPage : PopupPage
     {
+
         public DepartmentPopupPage()
         {
             InitializeComponent();
         }
+
+        private void LoadData()
+        {
+
+        }
+
+        private void GenerateCheckBox() 
+        {
+
+        }
+
+        async private void DepartmentConfirm_Clicked(object sender, 
+            EventArgs args) 
+        {
+            await Task.CompletedTask;
+        }
+        
     }
 }
