e63b2e7c47bd7dec437c25cd57242c16927fa0d6
nani0979 <52694220+nani0979@users.noreply.github.com>
2021-05-18 10:43:15 +0800

    start to hard code the wait bus stop functions.

---
+++ IndoorNavigation/IndoorNavigation/Modules/MonitorWaypoint.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IndoorNavigation.Modules
+{
+    // <summary>
+    // the class is for user to monitor the beacon in the bus, 
+    // and keep user not to leave the bus stop.
+    // </summary>
+    public class MonitorWaypoint:IDisposable
+    {
+
+        #region IDisposable support
+        private bool disposed = false;
+        protected virtual void Dispose(bool disposing) 
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    
+                }
+                disposed = true;
+            }
+        }
+        public void Dispose() 
+        {
+            Dispose(true);
+        }
+
+        #endregion
+    }
+}
