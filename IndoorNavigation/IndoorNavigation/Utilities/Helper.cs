563fed9633818181dd67d32d5880dd57dbfd28af
jasonchang <>
2021-06-16 16:15:03 +0800

    to finalize the theme.

--- IndoorNavigation/IndoorNavigation/Utilities/Helper.cs
+++ IndoorNavigation/IndoorNavigation/Utilities/Helper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-
+using static IndoorNavigation.Utilities.Constants;
+using Xamarin.Essentials;
 namespace IndoorNavigation.Utilities
 {
     public static class Helper
@@ -25,6 +26,17 @@ namespace IndoorNavigation.Utilities
             if (str.Trim().ToUpper() == "TRUE")
                 return true;
             return false;
+        }
+        public static string GetListTextColor(string userNaming)
+        {
+            if (userNaming.Trim().ToUpper().Contains(BETA_STRING)) return "#808A87";
+            switch (AppInfo.RequestedTheme){
+                case AppTheme.Dark:
+                    return "#f5f5f5";                    
+                case AppTheme.Light:
+                default:
+                    return "#3f51b5";
+            }
         }
     }
 }
